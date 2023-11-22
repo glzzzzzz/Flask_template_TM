c2eb9a2cb5576e997e4ebf643cc90d3e71fbd33e
glzzzzzz <135109408+glzzzzzz@users.noreply.github.com>
2023-11-22 21:46:11 +0100

    Ajout de la page calculateur de ration MAIS problème de javascript

--- app/views/pet.py
+++ app/views/pet.py
@@ -6,3 +6,17 @@ pet_bp = Blueprint('pet', __name__, url_prefix='/pet')
 @pet_bp.route('/mesanimaux',methods=['GET','POST'])
 def home_pet():
     return render_template('pet/mypets.html')
+
+
+
+@pet_bp.route('/calculateur_de_ration')
+def calculateur_de_ration():
+    return render_template('home/calculateur.html')
+
+@pet_bp.route('/calculateur_de_ration/cat_bee', methods=['GET','POST'])
+def cat_bee():
+    return render_template('pet/cat_bee.html')
+
+@pet_bp.route('/calculateur_de_ration/dog_bee', methods=['GET','POST'])
+def dog_bee():
+    return render_template('pet/dog_bee.html')
